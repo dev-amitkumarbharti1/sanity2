8691cdf937259880424c12d1d442356901ed5cc4
Amit Kumar Bharti <92047050+dev-amitkumarbharti1@users.noreply.github.com>
2023-02-10 18:08:53 +0530

    Initialize add-seo

---
+++ schemas/documents/page.ts
@@ -0,0 +1,80 @@
+import { DocumentIcon } from '@sanity/icons'
+import { defineArrayMember, defineField, defineType } from 'sanity'
+
+export default defineType({
+  type: 'document',
+  name: 'page',
+  title: 'Page',
+  icon: DocumentIcon,
+  fields: [
+    defineField({
+      type: 'string',
+      name: 'title',
+      title: 'Title',
+    }),
+    defineField({
+      type: 'slug',
+      name: 'slug',
+      title: 'Slug',
+      options: {
+        source: 'title',
+      },
+    }),
+    defineField({
+      name: 'overview',
+      description:
+        'Used both for the <meta> description tag for SEO, and the personal website subheader.',
+      title: 'Overview',
+      type: 'array',
+      of: [
+        // Paragraphs
+        defineArrayMember({
+          lists: [],
+          marks: {
+            annotations: [],
+            decorators: [
+              {
+                title: 'Italic',
+                value: 'em',
+              },
+              {
+                title: 'Strong',
+                value: 'strong',
+              },
+            ],
+          },
+          styles: [],
+          type: 'block',
+        }),
+      ],
+      validation: (rule) => rule.max(155).required(),
+    }),
+    defineField({
+      type: 'array',
+      name: 'body',
+      title: 'Body',
+      of: [
+        // Paragraphs
+        defineArrayMember({
+          type: 'block',
+        }),
+        // Custom blocks
+        defineArrayMember({
+          name: 'timeline',
+          type: 'timeline',
+        }),
+      ],
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'title',
+    },
+    prepare({ title }) {
+      return {
+        subtitle: 'Page',
+        title,
+      }
+    },
+  },
+})
